54ce845486933d2902cfd4e30e2d9ace56cebfa6
Max Rupnik <maxirupnik@gmail.com>
2024-11-25 11:44:15 +0100

    lean

--- 07-odvisni-tipi/vaje/logic.lean
+++ 07-odvisni-tipi/vaje/logic.lean
@@ -1,8 +1,15 @@
 -- Izomorfizmi
 
 theorem eq1 {A B : Prop} : (A ∧ B) ↔ (B ∧ A) :=
-  sorry
-
+  by
+    -- apply Iff.intro
+    constructor
+    intro ab
+    constructor
+    exact ab.right
+    exact ab.left
+    intro ba
+    sorry
 theorem eq2 {A B : Prop} : (A ∨ B) ↔ (B ∨ A) :=
   sorry
 
@@ -16,7 +23,25 @@ theorem eq5 {A B C : Prop} : A ∧ (B ∨ C) ↔ (A ∧ B) ∨ (A ∧ C) :=
   sorry
 
 theorem eq6 {A B C : Prop} : (B ∨ C) → A ↔ (B → A) ∧ (C → A) :=
-  sorry
+  by
+    constructor
+    intro h
+    constructor
+    intro b
+    apply h
+    left
+    exact b
+    intro c
+    have yy := h (Or.inr c)
+    exact yy
+    intro h BvC
+    cases BvC
+    case inl b =>
+      apply h.left
+      exact b
+    case inr c =>
+      apply h.right
+      exact c
 
 theorem eq7 {A B C : Prop} : C → (A ∧ B) ↔ (C → A) ∧ (C → B) :=
   sorry
--- 07-odvisni-tipi/vaje/vector.lean
+++ 07-odvisni-tipi/vaje/vector.lean
@@ -37,15 +37,27 @@ inductive Finite : Naravno -> Type where
 
 
 def lookup {A : Type} {n : Naravno} : Vektor A n -> Finite n -> A :=
-  sorry
+  fun xs i =>
+    match xs i =>
+    | Finite.fzero, Vektor.sestavljen y ys => y
+    | Finite.fsucc i', Vektor.sestavljen y ys => lookup ys i'
+
 
 
 -- Včasih enakost tipov ni takoj očitna in jo moramo izpeljati
 -- Dopolnite naslednjo definicijo, vse potrebne leme pa dokažite kar s taktiko `sorry`.
 
+
+
 def stakni_vektorja' : {A : Type} → {m n : Naravno} → Vektor A m → Vektor A n → Vektor A (plus n m) :=
   sorry
 
+def add_comm : {m n : Naravno} -> plus m n -> plus n m
+  := sorry
 -- Uporabite samo definicijo `stakni_vektorja'` in taktike `rw` in `exact`.
 def stakni_vektorja'' : {A : Type} → {m n : Naravno} → Vektor A m → Vektor A n → Vektor A (plus m n) :=
-  sorry
+  by
+    intro A m n xs ys
+    have xx := stakni_vektorja' xs ys
+    rw [add_comm] at xx
+    exact xx
